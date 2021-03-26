d0d08acd3e6cadcc44aaee96c1659f91a516b0e5
黄麟峰 <yzwshikee086@qq.com>
2021-03-26 15:42:44 +0800

    hello world.

---
+++ src/main/java/com/github/linfeng/App.java
@@ -0,0 +1,13 @@
+package com.github.linfeng;
+
+/**
+ * Hello world!
+ *
+ * @author 黄麟峰
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
